dfa5019fce7ff78f55fabfd8a54907d4dadb9e43
Wafelack <wafelack@protonmail.com>
2020-09-05 22:49:20 +0200

    Adding identify function and enum Source

--- src/install.rs
+++ src/install.rs
@@ -2,19 +2,32 @@ mod wanager;
 use std::fs;
 use std::io::{Error, ErrorKind};
 use std::path::Path;
-use wanager::Wanager;
+use wanager::*;
+
+fn identify(lib: &str) -> Source {
+    let splited: Vec<&str> = lib.split(':').collect();
+
+    match splited[0] {
+        "github" => return Source::GitHub(splited[1]),
+        "gitlab" => return Source::GitLab(splited[1]),
+        "bitbucket" => return Source::BitBucket(splited[1]),
+        &_ => return Source::Error(&format!("`{}` source is not valid !", splited[0])),
+    }
+}
 
 #[allow(unused_variables)]
 pub fn install(lib: &str) -> std::io::Result<()> {
     let w = Wanager;
-    if !w.exists(lib) {
-        return Err(Error::new(ErrorKind::NotFound, "Library does not exists !"));
-    }
-    let links = w.get_link(lib);
     if !Path::new("project.json").exists() {
         return Err(Error::new(ErrorKind::Other, "Not in a wanager project"));
     }
-    match w.install(lib, links) {
+
+    let source = match identify(lib) {
+        Source::Error(e) => return Err(Error::new(ErrorKind::Other, e)),
+        _ => identify(lib),
+    };
+
+    match w.install() {
         Ok(_) => (),
         Err(e) => println!("Library {} is already installed !", lib),
     }
--- src/install/wanager.rs
+++ src/install/wanager.rs
@@ -2,42 +2,21 @@ use std::process::Command;
 use std::str;
 
 pub struct Wanager;
+pub enum Source<'a> {
+    GitLab(&'a str),
+    GitHub(&'a str),
+    BitBucket(&'a str),
+    Error(&'a str),
+}
 
 impl Wanager {
-    pub fn exists(&self, lib_name: &str) -> bool {
-        let output = Command::new("curl")
-            .arg("https://wafelack.alwaysdata.net/libs.txt")
-            .output()
-            .unwrap();
-        let out = match str::from_utf8(&output.stdout) {
-            Ok(s) => s,
-            Err(_e) => return false,
-        };
-        let libs: Vec<&str> = out.split('\n').collect();
-        for l in libs {
-            if l == lib_name {
-                return true;
-            }
-        }
-        false
-    }
-    pub fn get_link(&self, lib_name: &str) -> (String, String) {
-        let header: String = format!("https://wafelack.alwaysdata.net/{}.h", lib_name);
-        let file: String = format!("https://wafelack.alwaysdata.net/{}.c", lib_name);
-        (header, file)
-    }
-    pub fn install(&self, lib_name:&str,  link: (String, String)) -> std::io::Result<()> {
+    pub fn install(&self, lib_name: &str, source: Source) -> std::io::Result<()> {
         let path: String = format!("src\\{}", lib_name);
         match std::fs::create_dir(&path) {
             Ok(_) => (),
             Err(e) => return Err(e),
         };
 
-        let full_head: String = format!("{}/{}.h", &path, lib_name);
-        let full_file: String = format!("{}/{}.c", &path, lib_name);
-
-        Command::new("curl").arg(link.0).arg("-o").arg(full_head).status().unwrap();
-        Command::new("curl").arg(link.1).arg("-o").arg(full_file).status().unwrap();
         Ok(())
     }
 }
