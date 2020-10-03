fa648f75e5f2070967449612817cf5162cc789af
Wafelack <wafelack@protonmail.com>
2020-10-03 20:58:38 +0200

    Good fixing install.rs

--- src/install.rs
+++ src/install.rs
@@ -1,6 +1,17 @@
 mod wanager;
 use std::path::Path;
-use wanager::Wanager;
+use wanager::*;
+
+fn identify(lib: &str) -> Source {
+    let splited: Vec<&str> = lib.split(':').collect();
+
+    match splited[0] {
+        "github" => return Source::GitHub(splited[1]),
+        "gitlab" => return Source::GitLab(splited[1]),
+        "bitbucket" => return Source::BitBucket(splited[1]),
+        &_ => return Source::Error("Invalid source"),
+    }
+}
 
 #[allow(unused_variables)]
 pub fn install(lib: &str) {
