a304b53b90f5d642d0a9c044e9eccc258467d958
Wafelack <wafelack@protonmail.com>
2021-01-27 20:47:11 +0100

    :art: Formatting code

--- wng-lib/src/config/reinit.rs
+++ wng-lib/src/config/reinit.rs
@@ -4,7 +4,8 @@ use std::{fs, fs::File};
 pub fn reinit(path: Option<&str>) -> crate::Result<()> {
     let home_dir = dirs::home_dir().unwrap();
     let default = format!("{}/wng.config", home_dir.to_str().unwrap());
-    let config_file = path.map(|x| x.to_owned()).unwrap_or(
+    let config_file = path.map(|x| x.to_owned())
+    .unwrap_or(
         default
     );
 
